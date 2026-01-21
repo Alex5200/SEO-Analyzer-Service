05dc405af78f5dc3ca2c6b770489704da62dd683
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2026-01-21 20:37:12 +0300

    Добавил TTL очистку кеша получение с .env

--- app/cache.py
+++ app/cache.py
@@ -2,6 +2,7 @@ import time
 from app.logger import logger
 from typing import Optional, Dict, Any
 from dataclasses import dataclass
+from app.config import CacheSettings
 
 
 @dataclass
@@ -9,12 +10,12 @@ class CacheEntry:
     data: Dict[str, Any]
     created_at: float
 
+
 class Cache:
-    def __init__(self, ttl_seconds: int = 300):
+    def __init__(self, ttl_seconds: int = 300) -> None:
         self._storage: Dict[str, CacheEntry] = {}
         self._ttl = ttl_seconds
         logger.debug("Кэш создан с TTL=%d seconds", ttl_seconds)
-
 
     def get(self, key: str) -> Optional[Dict[str, Any]]:
         if key not in self._storage:
@@ -34,10 +35,10 @@ class Cache:
         self._storage[key] = CacheEntry(data=value, created_at=time.time())
         logger.info("Кеш установлен по ключу: %s ", key)
 
-
     def clear(self) -> None:
         self._storage.clear()
         logger.info("Cache cleared ")
 
 
-cache = Cache(ttl_seconds=300)
+settings = CacheSettings()
+cache = Cache(ttl_seconds=settings.ttl_cache_seconds)
